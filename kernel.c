c7a749535e1e2c5ee4f505f9698a36d8f2d3d69e
Andy Qiang <daiqiang1117@hotmail.com>
2013-11-12 05:56:26 -0600

    finish step 3 reading a sector from disk

--- kernel.c
+++ kernel.c
@@ -4,63 +4,74 @@
 	Qiang Dai
 */
 
-/* Global Constant Variables*/
+/* macro */
 #define LENGTH_LINE 80
+#define LENGTH_SECTOR 512
 
 /* Function Prototypes*/
-void printString(char* word);
-void readString(char* line);
+void printString (char* word);
+void readString (char* line);
+void readSector (char* buffer, int sector);
+int mod (int dividend, int divisor);
+int div (int dividend, int divisor);
 
 
-
-/* Global Variables*/
-
+int j = 0;
 
 /* The main entry point of the program*/
-int main()
+int main ()
 {
 	/* todo
 	 * read a line from keyboard, maximum is 80 charaters
 	 * print this line to the screen
 	 */
-	char line[LENGTH_LINE];
-	printString("Enter a line: \0");
-	readString(line);
-	printString(line);
 
-	while(1)
+	/*step 2: read from keyboard*/
+	// char line[LENGTH_LINE];
+	// printString ("Enter a line: \0");
+	// readString (line);
+	// printString (line);
+
+	/*step 3: read a sector from disk*/
+	char buffer[LENGTH_SECTOR];
+	readSector (buffer, 30);
+	printString (buffer);
+
+	while (1)
 	{
 		
 	}
 	return 0;
 }
 
-void printString(char* word)
+void printString (char* word)
 {
-	/* C style string end with 0
+	/* Printing to Screen, Interrupt 0x10
+	 * C style string end with 0
 	 * if the character of word isn't 0 do loop
 	 * else return
 	 */
+
 	/* start at a new line*/
 	char al = '\n';
 	char ah = 0x0e;
 	int ax = ah * 256 + al;
 	interrupt(0x10, ax, 0, 0, 0);
 
-	while(*word != '\0')
+	while (*word != '\0')
 	{
 		/* INT 10/ AH = 0Eh - video - teletype output */
 		al = *word;
 		ax = ah * 256 + al;
-		interrupt(0x10, ax, 0, 0, 0);
+		interrupt (0x10, ax, 0, 0, 0);
 		word++;
 	}
 	return;
 }
 
-void readString(char* line)
+void readString (char* line)
 {
-	/* read a line from keyboard input
+	/* read a line from keyboard input, inpterrupt 0x 16
 	 * end with the ENTER key whose ASCII is 0xd
 	 * get this line and store in the line buffer
 	 * deal with backspace and end characters
@@ -68,20 +79,20 @@ void readString(char* line)
 
 	int i = 0;
 	char temp = 0;
-	while(1)
+	while (1)
 	{
-                /* the AH must equal 0 and AL doesn't matter */
-		temp = interrupt(0x16, 0, 0, 0, 0);
+        /* the AH must equal 0 and AL doesn't matter */
+		temp = interrupt (0x16, 0, 0, 0, 0);
 		/* only if the input is "ENTER", the loop ends*/
-		if( temp == 0xd)
+		if (temp == 0xd)
 			break;
 
 		/* if input is backspace, only diplay not store and index minus one */
-		if(temp == 0x8)
+		if (temp == 0x8)
 		{
-			if(i > 0)
+			if (i > 0)
 			{
-				interrupt(0x10, 0x0e * 256 + temp, 0, 0, 0);
+				interrupt (0x10, 0x0e * 256 + temp, 0, 0, 0);
 				i--;
 			}
 			else
@@ -92,14 +103,51 @@ void readString(char* line)
 		else
 		{
 			line[i++] = temp;
-			interrupt(0x10, 0x0e * 256 + temp, 0, 0, 0);	
+			interrupt (0x10, 0x0e * 256 + temp, 0, 0, 0);	
 		}
 	}
 
 	/* remeber to set the last two characters as 0xa and 0x0 */
-	if(i > LENGTH_LINE - 2)
+	if (i > LENGTH_LINE - 2)
 		i = LENGTH_LINE - 2;
 	line[i++] = 0xa;
 	line[i] = '\0';
 	return;
+}
+
+void readSector (char* buffer, int sector){
+	/*read a sector from disk, interrupt 0x13*/
+	/*
+	 * AH = 2 this number tells the BIOS to read
+	 * AL = number of sectors to read
+	 * BX = address where the data should be stored to
+	 * CH = track number
+	 * CL = relative sector number
+	 * DH = head number
+	 * DL = device number (for the floppy disk, use 0)*/
+	int ax = 2 * 256 + 1;
+	int bx = buffer;
+	int ch = div (sector, 36);
+	int cl = mod (sector, 18) + 1;
+	int cx = ch * 256 + cl;
+	int dx = mod (div (sector, 18), 2) * 256 + 0;
+
+	interrupt (0x13, ax, bx, cx, dx);
+
+	buffer[LENGTH_SECTOR - 1] = '\0';
+}
+
+int mod (int dividend, int divisor)
+{
+	while (dividend >= divisor)
+		dividend = dividend - divisor;
+	return dividend;
+}
+
+int div (int dividend, int divisor)
+{
+	int q = 0;
+	while (q * divisor <= dividend)
+		q = q + 1;
+	return q-1;
 }
